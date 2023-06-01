aaf255acccff3869a99838a04d91cc3c91f9be06
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-06-01 13:08:32 +0300

    dont allow to move where is pawn

--- table.js
+++ table.js
@@ -19,13 +19,19 @@ class Cell {
     let x2 = x1 + this.size;
     let y2 = y1 + this.size;
 
-    if (mouseX > x1 && mouseX < x2 && mouseY > y1 && mouseY < y2) {
+    if (
+      mouseX > x1 &&
+      mouseX < x2 &&
+      mouseY > y1 &&
+      mouseY < y2 &&
+      this.color == "brown"
+    ) {
       if (clickNumber % 2 == 0) {
         this.color = "blue";
+        console.log(x1, y1);
         clickNumber++;
       } else {
         this.color = "green";
-        console.log("plang");
         clickNumber++;
       }
     }
